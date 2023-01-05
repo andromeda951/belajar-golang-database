e60e452c576bf13d90f89e3641f169fda9c1b13f
Andromeda <andromeda951@gmail.com>
2023-01-05 14:42:38 +0700

    Tipe Data Column di Database

--- sql_test.go
+++ sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -40,4 +41,41 @@ func TestQuerySql(t *testing.T) {
 		fmt.Println("id:", id)
 		fmt.Println("name:", name)
 	}
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	sql := "SELECT id, name, email, balance, rating, brith_date, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx, sql)
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var id, name, email string
+		var balance int32
+		var rating float32
+		var birthDate, createAt time.Time
+		var married bool
+		err := rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createAt)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("=======================")
+		fmt.Println("id:", id)
+		fmt.Println("name:", name)
+		fmt.Println("email:", email)
+		fmt.Println("balance:", balance)
+		fmt.Println("rating:", rating)
+		fmt.Println("birth date:", birthDate)
+		fmt.Println("married:", married)
+		fmt.Println("create at:", createAt)
+	}
+
+	defer rows.Close()
+
 }
