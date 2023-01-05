72f434fc7d0ee1ea3a3b4e76ae7875b4b3ae8ab6
Andromeda <andromeda951@gmail.com>
2023-01-06 05:53:03 +0700

    SQL Injection Safe and SQL Parameter

--- sql_test.go
+++ sql_test.go
@@ -113,3 +113,48 @@ func TestSqlInjection(t *testing.T) {
 		fmt.Println("Gagal login")
 	}
 }
+
+func TestSqlInjectionSafe(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	
+	username := "admin'; #"
+	password := "admin"
+
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	rows, err := db.QueryContext(ctx, script, username, password)
+	if err != err {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Sukses login")
+	} else {
+		fmt.Println("Gagal login")
+	}
+}
+
+func TestExecSqlParameter(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "eko; DROP TABLE user; #"
+	password := "eko"
+	script := "INSERT INTO user(username, password) VALUES(?, ?);"
+	_, err := db.ExecContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success Insert New User")
+}
